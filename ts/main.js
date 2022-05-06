689c9cfc803c6eb1db11accd42f01a48432c1f5e
Hunter <46310167+hunterthis@users.noreply.github.com>
2022-05-06 15:29:04 -0700

    Got alert reptile to work, and fixed ts couldn't find js issuie

---
+++ ts/main.js
@@ -0,0 +1,8 @@
+"use strict";
+window.onload = function () {
+    let addBtn = document.querySelector("input[type=button]");
+    addBtn.onclick = addReptile;
+};
+function addReptile() {
+    alert("Reptile added");
+}
