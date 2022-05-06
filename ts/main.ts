f9c8c035a7c9c51d81df7905b7ad460ef66e6a10
Hunter <46310167+hunterthis@users.noreply.github.com>
2022-05-06 15:09:23 -0700

    first commit

---
+++ ts/main.ts
@@ -0,0 +1,11 @@
+window.onload = function() {
+    let addButton = document.querySelector("input[type=button]");
+    HTMLElement document.querySelector("input[type=button]");
+    addButton.click = addReptile;
+    if (Element instanceof HTMLElement) {
+        addButton.click();
+      }
+}
+function addReptile(){
+    alert("Reptile added";)
+}
